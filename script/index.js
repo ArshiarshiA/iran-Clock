b1fa160173ae48e3e0c6888aee09523174358175
arshia <marshia370@gmail.com>
2024-02-27 23:22:12 +0330

    coding a js minute counter

--- script/index.js
+++ script/index.js
@@ -1,9 +1,12 @@
 // DOM
 let second = document.querySelector('.second');
+let minute = document.querySelector('.minute');
 let deg = 6;
 setInterval(() => {
     let date = new Date();
     let secondNow = date.getSeconds() * 6;
+    let minuteNow = date.getMinutes() * 6;
 
-    second.style.transform = `rotateZ(${secondNow}deg)`
+    second.style.transform = `rotateZ(${secondNow}deg)`;
+    minute.style.transform = `rotateZ(${minuteNow}deg)`;
 }, 1000);
